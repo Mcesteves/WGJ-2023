34965bb8e512abbbaf72b3dd187feed01a01dd6e
Mcesteves <71904551+Mcesteves@users.noreply.github.com>
2023-08-12 16:48:10 -0300

    Implementa inventário e coleta de itens

---
+++ WGJ-2023/Assets/Scenes/Scripts/Inventory.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum item
+{
+    chalk,
+    frog,
+    broom,
+    candleBox,
+    blackPaint,
+    glitter,
+    hat,
+    key,
+    trophy,
+    plants,
+    stone,
+    stick,
+}
+
+[CreateAssetMenu(fileName = "InventorySO", menuName = "ScriptableObjects/Inventory")]
+public class InventorySO : ScriptableObject
+{
+    public bool chalk;
+    public bool frog;
+    public bool broom;
+    public bool candleBox;
+    public bool blackPaint;
+    public bool glitter;
+    public bool hat;
+    public bool key;
+    public bool trophy;
+    public bool plants;
+    public bool stone;
+    public bool stick;
+
+    public void LoadInventory()
+    {
+
+    }
+
+    public void Collect(item itemType)
+    {
+        switch (itemType)
+        {
+            case item.chalk:
+                chalk = true;
+                break;
+            case item.frog:
+                frog = true;
+                break;
+            case item.broom:
+                broom = true;
+                break;
+            case item.candleBox:
+                candleBox= true;
+                break;
+            case item.blackPaint:
+                blackPaint = true;
+                break;
+            case item.glitter:
+                glitter = true;
+                break;
+            case item.hat:
+                hat = true;
+                break;
+            case item.key:
+                key = true;
+                break;
+            case item.trophy:
+                trophy = true;
+                break;
+            case item.plants:
+                hat = true;
+                break;
+            case item.stone:
+                stone = true;
+                break;
+            case item.stick:
+                stick = true;
+                break;
+        }
+    }
+}
--- WGJ-2023/Assets/Scenes/Scripts/Moving and Collecting/CollectableItem.cs
+++ WGJ-2023/Assets/Scenes/Scripts/Moving and Collecting/CollectableItem.cs
@@ -3,13 +3,16 @@ using UnityEngine;
 
 public class CollectableItem : MovementManager
 {
+    public InventorySO inventory;
+    public item itemType;
     public override IEnumerator GoToPosition(Vector2 target)
     {
         yield return base.GoToPosition(target);
         movementStateSO.startPos = playerTransform.position;
         if (!interrupted)
         {
-            Debug.Log("coleta");
+            inventory.Collect(itemType);
+            Destroy(this.gameObject);
         }
         interrupted = false;
     }
