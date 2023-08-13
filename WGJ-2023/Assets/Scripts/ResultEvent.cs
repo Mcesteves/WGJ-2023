2fd0eeee6f49dd3e420f5321cc4ed3c4d503311c
MadudaJellyfish <105326941+MadudaJellyfish@users.noreply.github.com>
2023-08-13 19:12:45 -0300

    Prefabs criados

--- WGJ-2023/Assets/Scripts/ResultEvent.cs
+++ WGJ-2023/Assets/Scripts/ResultEvent.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
 
-public class NewBehaviourScript : MonoBehaviour
+public class ResultEvent : MonoBehaviour
 {
     public item[] conditionalItems;
     public Image resultStick;
