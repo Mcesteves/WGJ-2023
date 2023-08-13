f5a609a18efd98093aea66826002481d6594d3e6
Mcesteves <71904551+Mcesteves@users.noreply.github.com>
2023-08-12 22:51:36 -0300

    Organiza pastas e implementa exibição de mensagem

---
+++ WGJ-2023/Assets/Scripts/Moving and Collecting/Iteractable.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using TMPro;
+
+public class Iteractable : MovementManager
+{
+    public InventorySO inventory;
+    public string message;
+    [HideInInspector] public bool canInteract;
+    public item itemType;
+    [HideInInspector] public bool canCollect;
+
+    public static event Action<string> onShowMessage;
+    public override IEnumerator GoToPosition(Vector2 target)
+    {
+        yield return base.GoToPosition(target);
+        movementStateSO.startPos = playerTransform.position;
+        if (!interrupted && canInteract)
+        {
+            if (canCollect)
+            {
+                inventory.Collect(itemType);
+                Destroy(this.gameObject);
+            }
+            else
+            {
+                if(!UIManager.showingMessage)
+                    onShowMessage?.Invoke(message);
+            }
+        }
+        interrupted = false;
+    }
+
+}
