f7a565485b627e44814563ae97064e26e40f648d
Mcesteves <71904551+Mcesteves@users.noreply.github.com>
2023-08-13 21:22:43 -0300

    Implementa troca de sprite no botões de som

--- WGJ-2023/Assets/Scripts/Audio/AudioButton.cs
+++ WGJ-2023/Assets/Scripts/Audio/AudioButton.cs
@@ -1,22 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AudioButton : MonoBehaviour
 {
     private static string soundEffectsPrefs = "SFX";
     private static string musicPrefs = "Music";
     private int soundValueIdx;
+    public Sprite spriteOn;
+    public Sprite spriteOff;
 
     void Start()
     {
         SetSoundPrefs();
+        if (soundValueIdx == 0)
+            GetComponent<Image>().sprite = spriteOff;
+        else
+            GetComponent<Image>().sprite = spriteOn;
     }
 
     public void UpdateVolume()
     {
         soundValueIdx++;
         soundValueIdx %= 2;
+
+        if (soundValueIdx == 0)
+            GetComponent<Image>().sprite = spriteOff;
+        else
+            GetComponent<Image>().sprite = spriteOn;
 
         if (gameObject.tag == "Music")
             PlayerPrefs.SetInt(musicPrefs, soundValueIdx);
